f858b77c35827b0198341c46a3434904538f91ad
christenally <13616443+Christenally@users.noreply.github.com>
2026-04-15 23:57:50 -0500

    I hope this works

--- seePlusPlusFun.cpp
+++ seePlusPlusFun.cpp
@@ -1,21 +1,27 @@
-// seePlusPlusFun.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
+int main() 
 {
-std::cout << "Hello, world!";
-return 0;
+	//This is defining a variable
+	//int height;
+	//This is variable assignment
+	//height = 5;
+
+	//initialization - defining and assigning a initial value of an object
+	//int width { 5 };
+	/*
+	*width is the variable
+	*{5} is the initializer
+	*5 is the initial value
+	*/
+
+	//list-initialization does not allow narrowing-conversion
+	//this throws and error
+	int w1 { 4 };
+	//this compiles - should convert to 4?
+	std::cout << w1;
+	int w2 (4);
+	std::cout << w2;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
