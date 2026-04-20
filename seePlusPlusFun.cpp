1605c36b4c76e42b5cda14eaf95368b7a91137ad
christenally <13616443+Christenally@users.noreply.github.com>
2026-04-19 21:40:09 -0500

    practice

--- seePlusPlusFun.cpp
+++ seePlusPlusFun.cpp
@@ -17,11 +17,64 @@ int main()
 
 	//list-initialization does not allow narrowing-conversion
 	//this throws and error
-	int w1 { 4 };
+	//int w1 { 4 };
 	//this compiles - should convert to 4?
-	std::cout << w1;
-	int w2 (4);
-	std::cout << w2;
+	
 
+	//an unused variable will throw a compiler error
+	//[[maybe_unused]] tells the compiler that it's okay if it's unused
+	//[[maybe_unused]]int x{};
+
+	//[[maybe_unused]] int x{ 99 };
+
+	
+
+	
+	//std::cout << "Hi!" << std::endl;
+	//std::cout << "My name is Chris!" << std::endl;
+	
+	//'\n' is more efficient than using std::endl 
+	// '\n' does cause the buffer to be flushed when used
+
+
+
+	//int lucky{ 7 };
+	//std::cout << "Hi!\n";
+	//std::cout << "My name is Chris" << " and my favorite number is " << lucky << '/n';
+
+	//<< - insertion operator
+	//>> - extraction operator
+	//int x{};
+	//std::cout << "Enter a number!\n";
+	//std::cin >> x;
+	//std::cout << "The number is " << x;
+	//int a{};
+	//int b{};
+	//std::cout << "Enter two numbers separated by a space! ";
+	//std::cin >> a >> b;
+	//std::cout << "Your name is " << a << " " << b;
+	//
+	//
+	//return 0;
+
+	//int pi{ 3 };
+	//std::cout << "I have " << pi << " sodas";
+
+	//int age{ 34 };
+	//std::cout << "Hi!\n";
+	//std::cout << "My name is ChristenCodes!\n";
+	//std::cout << "I am " << age << " years old!\n";
+
+	int age{};
+	int date{};
+
+	std::cout << "Enter your age\n";
+	std::cin >> age;
+	std::cout << "Enter the todays date\n";
+	std::cin >> date;
+	std::cout << "Your are " << age << " years old" << "and todays date is " << date;
+
+	
+	
 	return 0;
 }
